5436a695b72d4e78b74f2b45e4e1db960f4a151c
shuta <chinoshuta@gmail.com>
2018-11-20 14:37:34 +0900

    11/20(火)feeds画面へのリンク追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,7 +4,7 @@ class FeedsController < ApplicationController
   # GET /feeds
   # GET /feeds.json
   def index
-    @feeds = Feed.all
+    @feeds = Feed.where('"feeds"."user_id" = ? ' , current_user.id)
   end
 
   # GET /feeds/1
